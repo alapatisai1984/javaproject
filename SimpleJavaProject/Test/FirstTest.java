b51895fd8ba9aae135577e50ce134621cf303678
gouthami <doddapaneni789@gmail.com>
2021-08-17 20:27:27 +0800

    Added test folder

---
+++ SimpleJavaProject/Test/FirstTest.java
@@ -0,0 +1,10 @@
+public class FirstTest
+{
+    public static void main(String[] args)
+    {
+        for(int i=0; i<=10; i++)
+        {
+            System.out.println("FirstTest... " + i);
+        }
+    }
+}
